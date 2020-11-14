d17bb4fdb4589ed6eb974323401d8d33797560b0
miraiy-outh <shokolatemeur@mail.ru>
2020-11-14 13:56:34 +0300

    2

--- src/com/company/Search.java
+++ src/com/company/Search.java
@@ -3,29 +3,29 @@ package com.company;
 import java.util.ArrayList;
 
 public class Search {
-    public ArrayList<Student> IDNumber = new ArrayList<>();
+    public ArrayList<Student> idNumber = new ArrayList<>();
 
     public Search(ArrayList<Student> IDNumber) {
-        this.IDNumber = IDNumber;
+        this.idNumber = IDNumber;
     }
     public int linSearch(Student obj) {
         int index = -1;
-        for (int i = 0; i < IDNumber.size(); i++) {
-            if (obj.equals(IDNumber.get(i))) {
+        for (int i = 0; i < idNumber.size(); i++) {
+            if (obj.equals(idNumber.get(i))) {
                 index = i + 1;
             }
         }
         return index;
     }
-    public int binSearch(Student key, int low, int high, int a) {
+    public int binSearch(int key, int low, int high, int a) {
         int mid = (low + high)/2;
-        if (IDNumber.indexOf(IDNumber.get(mid)) == IDNumber.indexOf(key)) {
+        if (idNumber.get(mid).equals(idNumber.get(key))) {
             return mid + 1;
         }
-        else if (IDNumber.indexOf(IDNumber.get(mid)) < IDNumber.indexOf(key)) {
+        else if (mid < key) {
             return binSearch(key, mid + 1, high, a);
         }
-        else if (IDNumber.indexOf(IDNumber.get(mid)) > IDNumber.indexOf(key)) {
+        else if (mid > key) {
             return binSearch(key, low, mid - 1, a);
         }
         return 0;
--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -3,33 +3,33 @@ package com.company;
 import java.util.*;
 
 public class Test {
-    public ArrayList<Student> IDNumber = new ArrayList<>();
+    public ArrayList<Student> idNumber = new ArrayList<>();
     Test() { };
     Test(int cnt) {
         Random rand = new Random();
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите данные студентов:");
         for (int i = 0; i < cnt; i++) {
-            IDNumber.add(new Student(scan.next(), scan.nextDouble(), scan.nextInt()));
+            idNumber.add(new Student(scan.next(), scan.nextDouble(), scan.nextInt()));
         }
 
-        Search s1 = new Search(IDNumber);
-        int r1 = rand.nextInt(IDNumber.size());
-        System.out.println("Искомый студент: " + IDNumber.get(r1).getName());
+        Search s1 = new Search(idNumber);
+        int r1 = rand.nextInt(idNumber.size());
+        System.out.println("Искомый студент: " + idNumber.get(r1).getName());
         long startTime1 = System.nanoTime();
-        int number1 = s1.linSearch(IDNumber.get(r1));
+        int number1 = s1.linSearch(idNumber.get(r1));
         long endTime1 = System.nanoTime();
         System.out.println("Номер искомого студента в коллекции: " + number1);
-        System.out.println(IDNumber.get(number1 - 1).toString());
+        System.out.println(idNumber.get(number1 - 1).toString());
 
-        Search s2 = new Search(IDNumber);
-        int r2 = rand.nextInt(IDNumber.size());
-        System.out.println("Искомый студент: " + IDNumber.get(r2).getName());
+        Search s2 = new Search(idNumber);
+        int r2 = rand.nextInt(idNumber.size());
+        System.out.println("Искомый студент: " + idNumber.get(r2).getName());
         long startTime2 = System.nanoTime();
-        int number2 = s1.binSearch(IDNumber.get(r2), 0, IDNumber.size(), -1);
+        int number2 = s1.binSearch(r2, 0, idNumber.size(), -1);
         long endTime2 = System.nanoTime();
         System.out.println("Номер искомого студента в коллекции: " + number2);
-        System.out.println(IDNumber.get(number2 - 1).toString());
+        System.out.println(idNumber.get(number2 - 1).toString());
 
         System.out.println("Время работы 1 метода поиска: " + (endTime1 - startTime1) + " миллисек.");
         System.out.println("Время работы 2 метода поиска: " + (endTime2 - startTime2) + " миллисек.");
